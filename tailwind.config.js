6b76d1c32850d06a8f371b6db984ff6831968f97
WinterBeyond <winterexodius@gmail.com>
2022-06-16 10:48:39 +0200

    Parse tailwind in constants

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx}",
 		"./src/components/**/*.{js,ts,jsx,tsx}",
+		"./src/constants/**/*.{js,ts,jsx,tsx}",
 	],
 	darkMode: "class",
 	theme: {
