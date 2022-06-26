9a2bc1928bd343184e386a4ebc66a2f0fd690bb1
WinterBeyond <winterexodius@gmail.com>
2022-06-26 13:12:09 +0200

    Final Google analytics fix

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,21 +4,24 @@ const Document = () => {
 	return (
 		<Html lang="en">
 			<Head>
-				<script
-					async
-					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-				/>
-				<script
-					dangerouslySetInnerHTML={{__html: `
-						window.dataLayer = window.dataLayer || [];
-						const gtag = () => {
-							window.dataLayer.push(arguments);
-						};
-				
-						gtag("js", new Date());
-						gtag("config", "${process.env.GOOGLE_ANALYTICS_ID}");
-					`}}
-				/>
+				{process.env.NODE_ENV === "production" && (
+					<>
+						<script
+							async
+							src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+						/>
+						<script
+							dangerouslySetInnerHTML={{
+								__html: `
+									window.dataLayer = window.dataLayer || [];
+									function gtag(){window.dataLayer.push(arguments);}
+									gtag("js", new Date());
+									gtag("config", "${process.env.GOOGLE_ANALYTICS_ID}");
+								`,
+							}}
+						/>
+					</>
+				)}
 			</Head>
 			<body>
 				<Main />
