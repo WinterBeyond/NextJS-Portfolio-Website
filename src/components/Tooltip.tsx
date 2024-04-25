7b300dcf5f0d915644022d25ccdf26d016b37532
WinterBeyond <winterexodius@gmail.com>
2024-04-25 16:49:42 +0200

    Improve tooltip position

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/common";
+
 type TooltipProps = {
 	visibleClass: string;
 	text: string;
@@ -6,7 +8,10 @@ type TooltipProps = {
 export default function Toolip({ visibleClass, text }: TooltipProps) {
 	return (
 		<div
-			className={`tooltip shadow-dark absolute bottom-10 ${visibleClass} z-50 hidden w-max rounded-lg border border-neutral-700 bg-neutral-900 bg-opacity-10 bg-clip-padding p-2 font-bold text-white backdrop-blur-lg backdrop-filter`}
+			className={cn(
+				"tooltip shadow-dark absolute bottom-10 right-0 z-50 hidden w-max rounded-lg border border-neutral-700 bg-neutral-900 bg-opacity-10 bg-clip-padding p-2 font-bold text-white backdrop-blur-lg backdrop-filter 2xl:right-[initial]",
+				visibleClass
+			)}
 		>
 			<p className="select-none text-sm leading-none">{text}</p>
 		</div>
