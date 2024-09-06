5b07a14b46aa62ea64a3a77b2e1ebf241bfd0883
WinterBeyond <winterexodius@gmail.com>
2024-09-06 19:10:39 +0200

    Update experiences

--- src/components/experience/experience-grid.tsx
+++ src/components/experience/experience-grid.tsx
@@ -5,12 +5,10 @@ export default async function ExperienceGrid() {
   const experiences = await getExperiences();
 
   return (
-    <div className="rtl grid gap-5 md:grid-cols-2 lg:grid-cols-3">
-      {experiences
-        .sort((experience) => (experience.endDate ? -1 : 1))
-        .map((experience) => (
-          <ExperienceCard key={experience.id} experience={experience} />
-        ))}
+    <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
+      {experiences.map((experience) => (
+        <ExperienceCard key={experience.id} experience={experience} />
+      ))}
     </div>
   );
 }
--- src/data-layer/experience.ts
+++ src/data-layer/experience.ts
@@ -6,15 +6,24 @@ const experiences: Array<Experience> = [
   {
     id: "cypoint",
     description:
-      "Working on a product information management system called Inriver, I create extensions and templates to allow the customer to more easily enrich their products. I also perform various other web development tasks with the help of React.",
+      "Creating automated workflows, building ecommerce websites and integrating with product information management systems. Building fullstack applications with React/NextJS.",
     company: "Cypoint Inspire",
     position: "Software Engineer",
     startDate: new Date(2022, 9),
   },
   {
+    id: "moonvive-entertainment",
+    description:
+      "Made important decisions on and deployed server infrastructure. I was also in-charge of the development of base functionality in the codebase.",
+    company: "Moonvive Entertainment",
+    position: "Backend Engineer",
+    startDate: new Date(2023, 1),
+    endDate: new Date(2023, 10),
+  },
+  {
     id: "northwood",
     description:
-      "Worked on internal tools, APIs, and server infrastructure. While working here I gained experience using C#, PHP, Redis and MySQL. I also gained experience in deploying to a production environment using continuous integration and continuous deployment practices.",
+      "Worked with internal tools, RESTful APIs, maintenance of Linux servers, and database operations.",
     company: "Northwood Studios",
     position: "System Administrator",
     startDate: new Date(2019, 2),
